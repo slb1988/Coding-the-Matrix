80a44ed1e41bd3c1582e916b8f346466bf0d6fb3
Quan Xin <quanxin1991@163.com>
2013-07-14 16:46:13 +0800

    Complete week 0; Add folder ./Resources and README

--- 03.hw0/hw0.py
+++ 03.hw0/hw0.py
@@ -5,55 +5,76 @@
 
 
 ## Problem 1
-def myFilter(L, num): pass
+def myFilter(L, num): return [i for i in L if i%num!=0]
 
 
 
 ## Problem 2
-def myLists(L): pass
+def myLists(L): return [list(range(1, i+1)) for i in L]
 
 
 
 ## Problem 3
-def myFunctionComposition(f, g): pass
+def myFunctionComposition(f, g): return {k:g[f[k]] for k in f.keys()}
 
 
 ## Problem 4
 # Please only enter your numerical solution.
 
-complex_addition_a = ... 
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = 5+3j
+complex_addition_b = 1j
+complex_addition_c = -1+0.001j
+complex_addition_d = 0.001+9j
 
 
 
 ## Problem 5
-GF2_sum_1 = ...
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
 
 ## Problem 6
-def mySum(L): pass
+def mySum(L):
+    sum = 0
+    for i in L:
+        sum += i
+    return sum
 
 
 
 ## Problem 7
-def myProduct(L): pass
+def myProduct(L):
+    pro = 1
+    for i in L:
+        pro *= i
+    return pro
 
 
 
 ## Problem 8
-def myMin(L): pass
+def myMin(L):
+    mini = L[0]
+    for i in L:
+        if i<mini:
+            mini = i
+    return mini
 
 
 
 ## Problem 9
-def myConcat(L): pass
+def myConcat(L):
+    con = ""
+    for i in L:
+        con += i
+    return con
 
 
 
 ## Problem 10
-def myUnion(L): pass
+def myUnion(L):
+    uni = set()
+    for i in L:
+        uni |= i
+    return uni
 
